ac21215c4541515151eefcd4bfcd1041638637bb
Diana Pinilla <dianii230416@gmail.com>
2025-03-06 16:18:01 -0500

    refactor: Delete unnecesary code

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -21,23 +21,9 @@ export const PokemonCard = ({pokemon}:PokemonCardProps) => {
             height={100}
             priority={false} // se coloca cuando se quiere que la carga de la imagen sea a demanda
           />
-          {/* <svg
-            aria-hidden="true"
-            role="img"
-            className="h-24 w-24 text-white rounded-full mx-auto"
-            width="32"
-            height="32"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 256 256"
-          >
-            <path
-              fill="currentColor"
-              d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"
-            ></path>
-          </svg> */}
           <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
           <div className="mt-5">
-            <Link href={`/dashboard/pokemon/${id}`} className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
+            <Link href={`/dashboard/pokemons/${name}`} className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
               Más información
             </Link>
           </div>
@@ -46,17 +32,6 @@ export const PokemonCard = ({pokemon}:PokemonCardProps) => {
           <Link className="px-4 py-2 hover:bg-gray-100 flex items-center" href="/dashboard/menu">
               <div className="text-red-600">
                 <IoHeartOutline/>
-                {/* <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5"
-                >
-                  <path d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                </svg> */}
               </div>
               <div className="pl-3">
                 <p className="text-sm font-medium text-gray-800 leading-none">
