1a2bf6213fe19051352bdc773aed1b948cc9ee0a
Diana Pinilla <dianii230416@gmail.com>
2025-03-06 16:24:42 -0500

    feat: Add api to get counter

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,8 +1,32 @@
 "use client";
-import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { addOne, initCounterState, substractOne } from "@/store/counter/counterSlice";
+import { useEffect } from "react";
+
+// siempre las cosas que tengan que ver con redux se manejan del lado del cliente, entonces toca ubicar los que necesitemos 
+//en un componente de useClient
+
+interface CounterResponse {
+  counter: number;
+  method:  string;
+}
+
+const getApiCounter = async () :Promise<CounterResponse>=>{
+  const data = await fetch('/api/counter').then(response => response.json())
+  return data;
+}
 
 export const CartCounter = () => {
-  const [count, setCount] = useState(10);
+
+  // se trae el count desde el estado
+  const count = useAppSelector(state => state.count.counter)
+  // se trae el dispatch para poder acceder a las acciones
+  const dispatch = useAppDispatch()
+
+  useEffect(()=>{
+    getApiCounter().then(({counter})=> dispatch(initCounterState(counter)) )
+  },[dispatch])
+
   return (
     <>
       <span className="text-9xl">{count}</span>
@@ -10,13 +34,13 @@ export const CartCounter = () => {
       <div className="flex gap-2">
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]"
-          onClick={() => setCount((prev) => prev + 1)}
+          onClick={() =>dispatch(addOne())}
         >
           +1
         </button>
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px]"
-          onClick={() => setCount((prev) => prev - 1)}
+          onClick={() => dispatch(substractOne())}
         >
           -1
         </button>
