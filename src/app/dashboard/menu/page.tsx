7b257372cea3dd14072c405a396e942566ad74e9
Diana Pinilla <dianii230416@gmail.com>
2025-03-06 16:18:19 -0500

    refactor: Reform page menu

--- src/app/dashboard/menu/page.tsx
+++ src/app/dashboard/menu/page.tsx
@@ -1,8 +1,18 @@
+import { WidgetsGrid } from "@/components";
+
+
+
+export const metadata = {
+ title: 'Admin dashboard',
+ description: 'Admin dashboard',
+};
 
 export default function MenuPage() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="text-black p-2">
+      <h1 className="text-3xl mt-2">Dashboard</h1>
+      <span className="text-xl">Información general</span>
+      <WidgetsGrid/>
     </div>
   );
 }
