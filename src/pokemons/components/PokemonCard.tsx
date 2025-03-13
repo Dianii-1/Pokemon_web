e10ee8eb3d0a5a047fd3f5ca99522675f190d335
Diana Pinilla <dianii230416@gmail.com>
2025-03-13 11:55:27 -0500

    refactor: Function toggle

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,45 +1,60 @@
+"use client";
 import Link from "next/link";
 import { SimplePokemon } from "../interfaces/simple-pokemon";
 import Image from "next/image";
-import {  IoHeartOutline } from "react-icons/io5";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { toggleFavorites } from "@/store/pokemons/pokemons";
 
-interface PokemonCardProps{
-    pokemon:SimplePokemon
+interface PokemonCardProps {
+  pokemon: SimplePokemon;
 }
-export const PokemonCard = ({pokemon}:PokemonCardProps) => {
+export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
+  const { id, name } = pokemon;
 
-    const {id,name} = pokemon;
+  //se utiliza la doble negacion para convertirlo en un boolean y no traer el objeto
+  const isFavorite = useAppSelector((state) => !!state.pokemons[id]);
+  const dispatch = useAppDispatch()
+
+  const onToggle = () => {
+    dispatch(toggleFavorites(pokemon))
+  }
 
   return (
     <div className="mx-auto right-0 mt-2 w-60">
       <div className="bg-white rounded overflow-hidden shadow-lg">
         <div className="flex flex-col justify-center items-center text-center p-6 bg-gray-800 border-b">
-        <Image
+          <Image
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
             alt={name}
             width={100}
             height={100}
             priority={false} // se coloca cuando se quiere que la carga de la imagen sea a demanda
           />
-          <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
+          <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">
+            {name}
+          </p>
           <div className="mt-5">
-            <Link href={`/dashboard/pokemons/${name}`} className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
+            <Link
+              href={`/dashboard/pokemons/${name}`}
+              className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
+            >
               Más información
             </Link>
           </div>
         </div>
         <div className="border-b">
-          <Link className="px-4 py-2 hover:bg-gray-100 flex items-center" href="/dashboard/menu">
-              <div className="text-red-600">
-                <IoHeartOutline/>
-              </div>
-              <div className="pl-3">
-                <p className="text-sm font-medium text-gray-800 leading-none">
-                  No es favorito
-                </p>
-                <p className="text-xs text-gray-500">View your campaigns</p>
-              </div>
-          </Link>
+          <div className="px-4 py-2 hover:bg-gray-100 flex items-center cursor-pointer" onClick={onToggle}>
+            <div className="text-red-600">
+              {isFavorite ? <IoHeart /> : <IoHeartOutline />}
+            </div>
+            <div className="pl-3">
+              <p className="text-sm font-medium text-gray-800 leading-none">
+                {isFavorite ? "Es favorito" : "No es favorito"}
+              </p>
+              <p className="text-xs text-gray-500">View your campaigns</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
